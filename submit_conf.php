52123973ce5310c2923bde4ee25545e86f20149c
gvshiva <venkatshiva.g0204@gmail.com>
2015-02-09 15:53:52 +0530

    successful_submit

--- submit_conf.php
+++ submit_conf.php
@@ -2,18 +2,35 @@
 <?php include 'header1.php'; ?>
 <?php error_reporting(E_STRICT ^ E_WARNING * E_DEPRECATED ^ E_NOTICE);?>
 <?php include 'tab_style.php'; ?>
-<div id="header">
+<!--
+<div id="header" >
   <ul>
     <li><a href="form1.php">Personal Details</a></li>
     <li><a href="form2.php">Publication Details</a></li>
     <li><a href="form3.php">Professional Activities</a></li>
     <li><a href="form4.php">SOP and LOR</a></li>
 	<!--<li><a href="../pdf_final.php">Submit</a></li>-->
-	<li id="current"><a href="submit.php">Submit</a></li>
+	<!--<li id="current"><a href="submit.php">Submit</a></li>
 	<li><a href="change-pwd.php">Change Password</a></li>
 	<a align='right' href='logout.php'>Logout</a>
   </ul>
 </div>
+-->
+<div class="row">
+  <div class="col-md-12 text-center">
+  <ul class="nav nav-tabs nav-justified">
+    <li><a href="form1.php">Personal Details</a></li>
+    <li><a href="form2.php">Publication Details</a></li>
+    <li><a href="form3.php">Professional Activities</a></li>
+    <li><a href="form4.php">SOP and LOR</a></li>
+	<!--<li><a href="../pdf_final.php">Submit</a></li>-->
+	<li class="active"><a href="submit.php">Submit</a></li>
+	<li><a href="change-pwd.php">Change Password</a></li>
+	<li><a href='logout.php'>Logout</a></li>
+  </ul>
+</div>
+</div>
+
 <br/><br/><br/><br/><br/>
 
 
